7de8e233de5ed3cc5c54ebcd42763a5fdfdc7891
Ezequias Rocha <ezequias.rocha@ccc.ufcg.edu.br>
2020-04-02 16:08:50 -0300

    validated input

--- scripts.js
+++ scripts.js
@@ -1,11 +1,25 @@
 let binaryInput = document.querySelector('#binary-input')
 let decimalOutput = document.querySelector('#decimal-output')
 
+function inputValidation(binary) {
+  for (value of binary.split('')) {
+    if (value !== '0' && value !== '1') return true
+  }
+  return false
+}
+
+function inputIsEmpty(binary) {
+  if (binary === '') return true
+  return false
+}
+
 function convertToBin(e) {
   e.preventDefault()
-  let decimalValue = parseInt(String(binaryInput.value), 2)
 
-  // console.log('. >> binaryValue: ', decimalValue)
+  if (inputValidation(binaryInput.value) || inputIsEmpty(binaryInput.value))
+    return alert('Please, enter a binary number')
+
+  let decimalValue = parseInt(String(binaryInput.value), 2)
 
   decimalOutput.value = decimalValue
 }
